98adbe9805e6c654471eba34e22d3210b09bd813
Wonyoung So <mail@wonyoung.so>
2019-07-09 01:07:23 +0100

    Updates

---
+++ precache-manifest.95677663e5827c8224b1b2a921a6575a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81585e8bff2c5af0b9993ffcb69e1005",
+    "url": "/index.html"
+  },
+  {
+    "revision": "870c78e4608334d99fa6",
+    "url": "/static/css/main.1ef19777.chunk.css"
+  },
+  {
+    "revision": "253c74a7cb1dba8abade",
+    "url": "/static/js/2.cdaf280d.chunk.js"
+  },
+  {
+    "revision": "870c78e4608334d99fa6",
+    "url": "/static/js/main.794aa9bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "519e8fa6e81315e236ca72151ee0d6fc",
+    "url": "/static/media/gerstnerprogramm-regular-webfont.519e8fa6.woff"
+  }
+]);
--- src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from 'redux';
-import mainReducer from './main';
-
-export default createStore(mainReducer);
